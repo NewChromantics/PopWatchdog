4fbe0a57271bb3b56eb809f163e5caa8b6eeb083
Graham Reeves <graham@grahamreeves.com>
2020-01-17 17:31:43 +0000

    Written Highlevel app for watchdog. Working!

--- PopWatchdog/bootup.js
+++ PopWatchdog/bootup.js
@@ -1,1 +1,18 @@
 Pop.Debug("Hello i am watchdog");
+
+//	todo: config file
+const RunCommand = "cmd /c msg %USERNAME% Configure Watchdog/Bootup.js to specify program to keep alive";
+const DelayBetweenRestartMs = 3000;
+
+async function RunForver()
+{
+	while (true)
+	{
+		const Program = new Pop.ShellExecute(RunCommand);
+		const ErrorCode = await Program.WaitForExit();
+		Pop.Debug(`[${RunCommand}] has exited (${ErrorCode}), waiting ${DelayBetweenRestartMs} ms before restarting...`);
+		await Pop.Yield(DelayBetweenRestartMs);
+	}
+}
+
+RunForver().then().catch(Pop.Debug);
